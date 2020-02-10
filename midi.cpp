9e51f2dd2c569314a266aa736eeb720ebb071295
Peter Russell <peter.russell4815@gmail.com>
2020-02-10 16:34:01 -0600

    Added MIDI tracking cababilities and started a USB debugger

--- midi.cpp
+++ midi.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <signal.h>
 #include "rtmidi/RtMidi.h"
 
 using namespace std;
+
+// Grab CTRL-C to end streams
+bool done;
+static void finish(int ignore) {done = true;}
 
 // Function prototypes
 void getOpenPorts(RtMidiIn*, RtMidiOut*);
 
 int main() {
+    // Container for midi messages
+    vector<unsigned char> messages;
+
     // These bois point to a midi port
     RtMidiIn *midiIn = nullptr;
     RtMidiOut *midiOut = nullptr;
@@ -28,6 +37,9 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Set up CTRL-C interupt signal
+    (void) signal(SIGINT, finish);
+
     // Initialize super dumb command prompt
     string word;
     string argument;
@@ -43,18 +55,33 @@ int main() {
             if (argument == "input")
                 try {
                     midiIn->openPort(portToOpen - 1);
+                    // Don't ignore sysex, time, or sense messages
+                    midiIn->ignoreTypes(false, false, false);
+                    cout << "Connected to input port " << portToOpen << endl;
                 } catch(RtMidiError &error) {
                     error.printMessage();
-                    goto cleanup;
                 }
             else if (argument == "output")
                 try {
                     midiOut->openPort(portToOpen - 1);
+                    cout << "Connected to output port " << portToOpen;
                 } catch(RtMidiError &error) {
                     error.printMessage();
-                    goto cleanup;
                 }
             else cout << "Invalid command" << endl;
+        }
+        else if (word == "track") {
+            done = false;
+            while (!done) {
+                // Read in midi signal into buffer
+                double stamp = midiIn->getMessage(&messages);
+                int size = messages.size();
+                for (int i = 0; i < size; i++) {
+                    cout << "Byte " << i << ": " << (int)messages.at(i) << endl;
+                }
+                if (size > 0) cout << "Stamp: " << stamp << endl << endl;
+                messages.clear();
+            }
         }
         else cout << "Invalid command" << endl;
     } while(word != "exit");
---
+++ usb.cpp
@@ -0,0 +1,5 @@
+#include <libusb-1.0/libusb.h>
+
+int main() {
+    return 0;
+}
